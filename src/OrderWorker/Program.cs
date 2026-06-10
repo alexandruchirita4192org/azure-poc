3fe6aad72e7dbf35a93b4417c6eac5ddb88c898f
Alexandru Chirita <Alexandru.Chirita2@endava.com>
2026-06-10 10:51:54 +0300

    Fix build

--- src/OrderWorker/Program.cs
+++ src/OrderWorker/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Azure.Monitor.OpenTelemetry.AspNetCore;
 
 var host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((context, services) =>
