7a5897d455994ed850ca7fd11dd96b6a42e35bd9
Alexandru Chirita <Alexandru.Chirita2@endava.com>
2026-06-10 10:37:29 +0300

    Add 'Azure.Monitor.OpenTelemetry.AspNetCore' to make build work

--- src/OrderApi/Program.cs
+++ src/OrderApi/Program.cs
@@ -7,6 +7,7 @@ using Azure.Messaging.ServiceBus;
 using Azure.Storage.Blobs;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Data.SqlClient;
+using Azure.Monitor.OpenTelemetry.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
